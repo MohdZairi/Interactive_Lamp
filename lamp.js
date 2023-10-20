ce5847e572383d6a06d6285b29078110f7cc830f
MohdZairi <mohdzairi658@gmail.com>
2023-10-20 22:24:31 +0800

    first commit

---
+++ lamp.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const lamps = document.querySelectorAll(".lamp");
+
+    lamps.forEach(lamp => {
+        lamp.addEventListener("click", function () {
+            lamp.classList.toggle("off");
+            updateBackground();
+        });
+    });
+
+    function updateBackground() {
+        const body = document.body;
+        const lampsOn = document.querySelectorAll(".lamp:not(.off)").length;
+
+        if (lampsOn === 2) {
+            // Change background to white when both lamps are on
+            body.style.background = "#f2e34d"; // White background
+        }
+        else
+        {
+            // Change background to white when both lamps are on
+            body.style.background = "#000"; // black background
+        }
+        if (lampsOn === 1) {
+            // Change background to half black and half yellow when one lamp is on and one lamp is off
+            body.style.background = "linear-gradient(90deg, #000 50%, #f2e34d 50%)";
+        }
+    }
+});
